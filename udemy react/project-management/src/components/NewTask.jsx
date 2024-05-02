273302e1661aa48b49aeeefafcd199b66c546162
Rishni Meemeduma <34571667+RishniMeemeduma@users.noreply.github.com>
2024-05-03 02:58:50 +0530

    Delete udemy react directory

--- udemy react/project-management/src/components/NewTask.jsx
+++
@@ -1,8 +0,0 @@
-export default function NewTask() {
-    return (
-        <div className="flex item-center gap-4">
-            <input type="text" className="w-64 px-2 py-1 rounded-sm bg-stone-200"  />
-            <button className="text-stone-700 hover:text-stone-900">Add Task</button>
-        </div>
-    )
-}
